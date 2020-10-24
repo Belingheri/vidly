8985a57b8f7c828975d023df1234f0b1b7d277e8
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-24 14:58:03 +0200

    crezione componente x gestire freccia ord

---
+++ src/model/common/arrowOrder.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ArrowOrder(props) {
+  const { order } = props;
+
+  if (order !== "asc" && order !== "desc") return null;
+
+  const getClass = () => {
+    let className = "fa fa-arrow-";
+    className += order === "asc" ? "down" : "up";
+    return className;
+  };
+
+  return <i className={getClass()} aria-hidden="true"></i>;
+}
+
+ArrowOrder.propTypes = {
+  order: PropTypes.string.isRequired,
+};
+
+export default ArrowOrder;
