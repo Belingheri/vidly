2ec76ce5130d340aa2d3141d3b717055857af99f
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-24 16:37:16 +0200

    Create component TableBody

---
+++ src/model/common/tableBody.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+function TableBody(props) {
+  const { data, columns } = props;
+
+  const renderCell = (row, item) => {
+    if (item.content) return item.content(row);
+
+    return _.get(row, item.path);
+  };
+
+  return (
+    <tbody>
+      {data.map((row) => (
+        <tr key={uuidv4()}>
+          {columns.map((item) => (
+            <td key={uuidv4()}>{renderCell(row, item)}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+TableBody.propTypes = {
+  data: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+};
+
+export default TableBody;
