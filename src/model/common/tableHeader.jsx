226469bb0f1c02073e682ad9b1e3fb39d2e78536
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-24 15:55:57 +0200

    create component

---
+++ src/model/common/tableHeader.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
+
+import { invertSortType } from "../../util/sorter";
+
+import ArrowOrder from "./arrowOrder";
+
+function TableHeader(props) {
+  const { hearersField, sortedColumn, onSort } = props;
+
+  const handleSort = (path) => {
+    let newSortedColumn = { ...sortedColumn };
+    if (path === newSortedColumn.path)
+      newSortedColumn.order = invertSortType(newSortedColumn.order);
+    else newSortedColumn = { path, order: "asc" };
+    onSort(newSortedColumn);
+  };
+
+  return (
+    <thead>
+      <tr>
+        {hearersField.map((el) => (
+          <th
+            key={uuidv4()}
+            onClick={() => {
+              if (el.key) handleSort(el.key);
+            }}
+          >
+            {el.value}
+            <ArrowOrder
+              order={sortedColumn.path === el.key ? sortedColumn.order : ""}
+            />
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
+
+TableHeader.propTypes = {
+  hearersField: PropTypes.array.isRequired,
+  sortedColumn: PropTypes.object.isRequired,
+  onSort: PropTypes.func.isRequired,
+};
+
+export default TableHeader;
