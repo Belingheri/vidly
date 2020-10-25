a5c8de472406abfbd2c003f44144f2dc1439e7e0
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-25 14:44:18 +0100

    implementato routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 //import logo from "./logo.svg";
 import "./App.css";
-import Movies from "./components/movies";
+
+import NotFound from "./components/common/notFound";
+
+import NavBar from "./components/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import MoviesRoute from "./components/moviesRoute";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={MoviesRoute} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="not-found" />
+        </Switch>
       </main>
     </div>
   );
