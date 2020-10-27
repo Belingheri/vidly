437340403bd280b1d7334847f45650f1d27a5915
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-27 13:58:55 +0100

    fix descrizioni errate

--- src/components/movieInsert.jsx
+++ src/components/movieInsert.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
 import MovieForm from "./movieForm";
 
 function MovieInsert() {
-  const { id } = useParams();
-
   return (
     <div className="row">
       <div className="col">
         <h1>Crea</h1>
-        <MovieForm id={id} />
+        <MovieForm />
       </div>
     </div>
   );
