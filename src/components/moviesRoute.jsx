ae34f1ee8c630ba7dcbafdcfb9afdd8b155d7b44
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-10-27 13:51:53 +0100

    add MovieInsert component

--- src/components/moviesRoute.jsx
+++ src/components/moviesRoute.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Movies from "./movies";
-import MovieForm from "./movieForm";
+import MovieInsert from "./movieInsert";
 import MovieUpdate from "./movieUpdate";
 
 function MoviesRoute() {
   return (
     <Switch>
-      <Route path="/movies/new" component={MovieForm} />
+      <Route path="/movies/new" component={MovieInsert} />
       <Route path="/movies/:id" component={MovieUpdate} />
       <Route path="/movies" component={Movies} />
     </Switch>
